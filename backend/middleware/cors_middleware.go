35c709ef9653bd635332ede044830af902dc26ed
Peter-SB <peterboughton11@live.co.uk>
2024-12-02 17:10:25 +0000

    Add CORS middleware

---
+++ backend/middleware/cors_middleware.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+// CORS Middleware
+// This is needed because the back-end and front-end are on different ports
+func CORSMiddleware(allowedOrigins []string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			origin := r.Header.Get("Origin")
+
+			// Check if the origin is in the allowed list
+			for _, o := range allowedOrigins {
+				if o == origin {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+					w.Header().Set("Access-Control-Allow-Credentials", "true") // Enable because using cookies and session-based auth
+					break
+				}
+			}
+
+			// Handle preflight requests
+			if r.Method == http.MethodOptions {
+				w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+				w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+				w.WriteHeader(http.StatusNoContent)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
--- backend/server/routes.go
+++ backend/server/routes.go
@@ -1,11 +1,21 @@
 package server
 
-import "net/http"
+import (
+	"go-chat-app/middleware"
+
+	"net/http"
+)
 
 func SetupRoutes() {
-	http.HandleFunc("/history", GetChatHistoryHandler)
+	// Define allowed origins
+	allowedOrigins := []string{
+		"http://localhost:3000",
+	}
 
-	http.HandleFunc("/ws", HandleConnections)
+	corsMiddleware := middleware.CORSMiddleware(allowedOrigins)
+
+	http.Handle("/history", corsMiddleware(http.HandlerFunc(GetChatHistoryHandler)))
+	http.Handle("/ws", corsMiddleware(http.HandlerFunc(HandleConnections)))
 
 	http.HandleFunc("/register", Register)
 	http.HandleFunc("/login", LoginUser)
